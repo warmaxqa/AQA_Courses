fc80b611eee8f3fbd690f0a662d85f1de08b536b
Max KVSKY <warmroller@gmail.com>
2024-07-30 10:44:44 +0300

    homework 5 is ready

---
+++ Lesson_05/Homework 5.2.py
@@ -0,0 +1,28 @@
+# Список кортежів
+
+people_records = [
+    ('John', 'Doe', 28, 'Engineer', 'New York'),
+    ('Alice', 'Smith', 35, 'Teacher', 'Los Angeles'),
+    ('Bob', 'Johnson', 45, 'Doctor', 'Chicago'),
+    ('Emily', 'Williams', 30, 'Artist', 'San Francisco'),
+    ('Michael', 'Brown', 22, 'Student', 'Seattle'),
+    ('Sophia', 'Davis', 40, 'Lawyer', 'Boston'),
+    ('David', 'Miller', 33, 'Software Developer', 'Austin'),
+    ('Olivia', 'Wilson', 27, 'Marketing Specialist', 'Denver'),
+    ('Daniel', 'Taylor', 38, 'Architect', 'Portland'),
+    ('Grace', 'Moore', 25, 'Graphic Designer', 'Miami'),
+    ('Samuel', 'Jones', 50, 'Business Consultant', 'Atlanta'),
+    ('Emma', 'Hall', 31, 'Chef', 'Dallas'),
+    ('William', 'Clark', 29, 'Financial Analyst', 'Houston'),
+    ('Ava', 'White', 42, 'Journalist', 'San Diego'),
+    ('Ethan', 'Anderson', 36, 'Product Manager', 'Phoenix')
+]
+
+added_new_people = ('Max', "Kudriavskiy", 32, "Musician", "Kyiv")
+people_records.insert(0,added_new_people)
+
+people_records[1], people_records[5] = people_records[5], people_records[1]
+
+print("Update_list:")
+for record in people_records:
+    print(record)
