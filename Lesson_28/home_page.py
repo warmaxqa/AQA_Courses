9d25d32199c30d505a7b3a31318ee8c264071ccb
Max KVSKY <warmroller@gmail.com>
2024-10-23 14:50:00 +0300

    Fixed text characters

--- Lesson_28/home_page.py
+++ Lesson_28/home_page.py
@@ -4,9 +4,7 @@ from pages.base_page import BasePage
 class HomePage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
-        self.registration_button = (By.CSS_SELECTOR, "a[href='#signup']")  # Кнопка реєстрації
-        self.login_button = (By.CSS_SELECTOR, "a[href='#login']")  # Кнопка входу
-        self.profile_button = (By.CSS_SELECTOR, ".profile-btn")  # Кнопка профілю після авторизації
+        self.registration_button = (By.CSS_SELECTOR, "a[href='https://UserName:Password@qauto2.forstudy.space']")
 
     def click_registration_button(self):
         self.wait_for_element(self.registration_button).click()
